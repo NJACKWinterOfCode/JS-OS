9f21df08d241edb1caae2c08f9b0eba8dea0b76f
ankingcodes <ankush.bhardwaj0@gmail.com>
2020-01-04 23:44:08 +0530

    added update route

--- src/server/routes/filesystem.js
+++ src/server/routes/filesystem.js
@@ -43,4 +43,16 @@ router.delete('/deleteFile', function(req,res){
     })
 })
 //deleting a directory
+
+
+
+
+//updating a file
+router.put('/updateFile', function(req,res){
+    console.log(req.body)
+    db.File.findOneAndUpdate(req.body)
+    .then(function(f){
+        res.json(f)
+    })
+})
 module.exports = router
